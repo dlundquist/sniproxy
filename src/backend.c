980c588a5d43c9f7ff902e1cab84d0e9710bc6e5
Dustin Lundquist <dustin@null-ptr.net>
2011-08-24 21:36:26 -0700

    Set _POSIX_SOURCE, sockfd instead of s

--- src/backend.c
+++ src/backend.c
@@ -76,49 +76,49 @@ lookup_backend(const char *hostname) {
 
 static int
 open_backend_socket(struct Backend *b, const char *req_hostname) {
+    int sockfd = -1, error;
     struct addrinfo hints, *res, *res0;
-    int error;
-    int s;
     const char *cause = NULL;
-    char portstr[8];
+    char portstr[6]; /* port numbers are < 65536 */
 
     const char *target_hostname = b->address;
     if (strcmp(target_hostname, "*") == 0)
         target_hostname = req_hostname;
 
-    snprintf(portstr, 8, "%d", b->port);
+    snprintf(portstr, 6, "%d", b->port);
     syslog(LOG_DEBUG, "Connecting to %s:%s", target_hostname, portstr);
 
     memset(&hints, 0, sizeof(hints));
     hints.ai_family = PF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
+
     error = getaddrinfo(target_hostname, portstr, &hints, &res0);
-    if (error) {
+    if (error != 0) {
         syslog(LOG_NOTICE, "Lookup error: %s", gai_strerror(error));
         return -1;
     }
-    s = -1;
+
     for (res = res0; res; res = res->ai_next) {
-        s = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-        if (s < 0) {
+        sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+        if (sockfd < 0) {
             cause = "socket";
             continue;
         }
 
-        if (connect(s, res->ai_addr, res->ai_addrlen) < 0) {
+        if (connect(sockfd, res->ai_addr, res->ai_addrlen) < 0) {
             cause = "connect";
-            close(s);
-            s = -1;
+            close(sockfd);
+            sockfd = -1;
             continue;
         }
 
         break;  /* okay we got one */
     }
-    if (s < 0)
+    if (sockfd < 0)
         syslog(LOG_ERR, "%s error: %s", cause, strerror(errno));
 
     freeaddrinfo(res0);
-    return s;
+    return sockfd;
 }
 
 void
