da81020d1a06baf4a891761f9d5294c4114dcf90
Dustin Lundquist <dustin@null-ptr.net>
2011-08-29 18:00:08 -0700

    More robust perl test_proxy script

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -5,4 +5,7 @@ while read TEST
 do
     ${TEST} || (echo "Failure in test ${TEST}"; exit 1)
 done
+
+./test_proxy || (echo "Failure in functional test"; exit 1);
+
 echo "All tests completed successfully"
