b8d8b4ddd80ce3d40f142d4571730e9e9c2d8aec
Dustin Lundquist <dustin@null-ptr.net>
2011-12-22 18:57:30 -0800

    Use union to reduce casts

--- src/util.c
+++ src/util.c
@@ -41,28 +41,35 @@ int isnumeric (const char * s)
 }
 
 size_t
-parse_address(struct sockaddr_storage* addr, const char* address, int port) {
+parse_address(struct sockaddr_storage* saddr, const char* address, int port) {
+    union {
+        struct sockaddr_storage *storage;
+        struct sockaddr_in *sin;
+        struct sockaddr_in6 *sin6;
+        struct sockaddr_un *sun;
+    } addr;
+    addr.storage = saddr;
 
-    memset(addr, 0, sizeof(struct sockaddr_storage));
-    if (inet_pton(AF_INET, address, &(((struct sockaddr_in *)addr)->sin_addr)) == 1) {
-        ((struct sockaddr_in *)addr)->sin_family = AF_INET;
-        ((struct sockaddr_in *)addr)->sin_port = htons(port);
+    memset(addr.storage, 0, sizeof(struct sockaddr_storage));
+    if (inet_pton(AF_INET, address, &addr.sin->sin_addr) == 1) {
+        addr.sin->sin_family = AF_INET;
+        addr.sin->sin_port = htons(port);
         return sizeof(struct sockaddr_in);
     }
 
-    memset(addr, 0, sizeof(struct sockaddr_storage));
-    if (inet_pton(AF_INET6, address, &(((struct sockaddr_in6 *)addr)->sin6_addr)) == 1) {
-        ((struct sockaddr_in6 *)addr)->sin6_family = AF_INET6;
-        ((struct sockaddr_in6 *)addr)->sin6_port = htons(port);
+    /* rezero addr incase inet_pton corrupted it while trying to parse IPv4 */
+    memset(addr.storage, 0, sizeof(struct sockaddr_storage));
+    if (inet_pton(AF_INET6, address, &addr.sin6->sin6_addr) == 1) {
+        addr.sin6->sin6_family = AF_INET6;
+        addr.sin6->sin6_port = htons(port);
         return sizeof(struct sockaddr_in6);
     }
 
+    memset(addr.storage, 0, sizeof(struct sockaddr_storage));
     if (strncmp("unix:", address, 5) == 0) {
-        memset(addr, 0, sizeof(struct sockaddr_storage));
-        ((struct sockaddr_un *)addr)->sun_family = AF_UNIX;
-        strncpy(((struct sockaddr_un *)addr)->sun_path, address + 5, UNIX_PATH_MAX);
-
-        return offsetof(struct sockaddr_un, sun_path) + strlen(((struct sockaddr_un *)addr)->sun_path);
+        addr.sun->sun_family = AF_UNIX;
+        strncpy(addr.sun->sun_path, address + 5, UNIX_PATH_MAX);
+        return offsetof(struct sockaddr_un, sun_path) + strlen(addr.sun->sun_path);
     }
 
     return 0;
