ccd467d76b678a26fca284114e28ef765f311f2a
Dustin Lundquist <dustin@null-ptr.net>
2013-04-17 19:49:29 -0700

    deleting old script

--- tests/run_tests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-find . -name \*_test -perm -0700 -type f | \
-while read TEST
-do
-    ${TEST} || (echo "Failure in test ${TEST}"; exit 1)
-done
-
-echo "All tests completed successfully"
