a01c5b8947dca1205572fb5016e3db6d2d4c8178
Dustin Lundquist <dustin@null-ptr.net>
2013-08-31 17:07:10 -0700

    HTTP parser updated

--- tests/http_test.c
+++ tests/http_test.c
@@ -14,6 +14,11 @@ const char *good[] = {
         "HOST:\t     localhost\r\n"
         "Accept: */*\r\n"
         "\r\n",
+    "GET / HTTP/1.1\r\n"
+        "User-Agent: curl/7.21.0 (x86_64-pc-linux-gnu) libcurl/7.21.0 OpenSSL/0.9.8o zlib/1.2.3.4 libidn/1.18\r\n"
+        "HOST:\t     localhost:8080\r\n"
+        "Accept: */*\r\n"
+        "\r\n",
 };
 const char *bad[] = {
     "GET / HTTP/1.0\r\n"
@@ -25,30 +30,41 @@ const char *bad[] = {
         "\n",
     "GET / HTTP/1.1\r\n"
         "User-Agent: curl/7.21.0 (x86_64-pc-linux-gnu) libcurl/7.21.0 OpenSSL/0.9.8o zlib/1.2.3.4 libidn/1.18\r\n"
-        "Host: xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-        "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-        "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-        "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-        "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-        "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\r\n"
+        "Hostname: localhost\r\n"
+        "Accept: */*\r\n"
+        "\r\n",
+    "GET / HTTP/1.1\r\n"
+        "User-Agent: curl/7.21.0 (x86_64-pc-linux-gnu) libcurl/7.21.0 OpenSSL/0.9.8o zlib/1.2.3.4 libidn/1.18\r\n"
         "Accept: */*\r\n"
         "\r\n",
 };
 
 int main() {
     unsigned int i;
+    int result;
     const char *hostname;
 
     for (i = 0; i < sizeof(good) / sizeof(const char *); i++) {
-        hostname = parse_http_header(good[i], strlen(good[i]));
+        hostname = NULL;
+
+        result = parse_http_header(good[i], strlen(good[i]), &hostname);
+
+        assert(result == 9);
 
         assert(NULL != hostname);
 
         assert(0 == strcmp("localhost", hostname));
     }
 
-    for (i = 0; i < sizeof(bad) / sizeof(const char *); i++)
-        hostname = parse_http_header(bad[i], strlen(bad[i]));
+    for (i = 0; i < sizeof(bad) / sizeof(const char *); i++) {
+        hostname = NULL;
+
+        result = parse_http_header(bad[i], strlen(bad[i]), &hostname);
+
+        assert(result < 0);
+
+        assert(hostname == NULL);
+    }
 
     return 0;
 }
