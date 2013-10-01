b370ec78e14328744261e594edbde2cbf946038b
Dustin Lundquist <dustin@null-ptr.net>
2013-09-30 17:29:32 -0700

    Remove debug syslog messages on backend matching

--- src/backend.c
+++ src/backend.c
@@ -112,14 +112,10 @@ lookup_backend(const struct Backend_head *head, const char *hostname) {
     if (hostname == NULL)
         hostname = "";
 
-    STAILQ_FOREACH(iter, head, entries) {
-        if (pcre_exec(iter->hostname_re, NULL, hostname, strlen(hostname), 0, 0, NULL, 0) >= 0) {
-            syslog(LOG_DEBUG, "%s matched %s", iter->hostname, hostname);
+    STAILQ_FOREACH(iter, head, entries)
+        if (pcre_exec(iter->hostname_re, NULL, hostname, strlen(hostname), 0, 0, NULL, 0) >= 0)
             return iter;
-        } else {
-            syslog(LOG_DEBUG, "%s didn't match %s", iter->hostname, hostname);
-        }
-    }
+
     return NULL;
 }
 
@@ -153,7 +149,6 @@ open_backend_socket(struct Backend *b, const char *req_hostname) {
         target_hostname = req_hostname;
 
     snprintf(portstr, sizeof(portstr), "%d", b->port);
-    syslog(LOG_DEBUG, "Connecting to %s:%s", target_hostname, portstr);
 
     memset(&hints, 0, sizeof(hints));
     hints.ai_family = PF_UNSPEC;
