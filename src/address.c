93e8027797a9a4aec96a0d366e114947ce0650b2
Dustin Lundquist <dustin@null-ptr.net>
2013-10-13 14:44:00 -0700

    Include netinet/in.h for struct sockaddr_in

--- src/address.c
+++ src/address.c
@@ -26,8 +26,10 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
-#include <arpa/inet.h> /* inet_pton */
+#include <sys/types.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h> /* inet_pton */
 #include <sys/un.h>
 #include <assert.h>
 #include "address.h"
