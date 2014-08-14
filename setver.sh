62f0bab4ec49f3c9d70ce341a25109daa5350891
Dustin Lundquist <dustin@null-ptr.net>
2014-08-13 17:30:54 -0700

    Fix setver.sh handling of Debian release versions

--- setver.sh
+++ setver.sh
@@ -11,13 +11,19 @@ cd ${SOURCE_DIR}
 if [ -d ${GIT_DIR} ]; then
     GIT_VERSION=$(git describe --tags)
     if [ "x" != "x${GIT_VERSION}" ]; then
-        VER=$(echo ${GIT_VERSION} | cut -d- -f1)
-        REV=$(echo ${GIT_VERSION} | cut -d- -f2)
-        REF=$(echo ${GIT_VERSION} | cut -d- -f3)
+        if echo ${GIT_VERSION} | grep -q '-'; then
+            VER=$(echo ${GIT_VERSION} | cut -d- -f1)
+            REV=$(echo ${GIT_VERSION} | cut -d- -f2)
+            REF=$(echo ${GIT_VERSION} | cut -d- -f3)
 
-        VERSION=${GIT_VERSION}
-        # Debian versions for native packages can not contain hyphens
-        DEBIAN_VERSION=${VER}+git${REV}.${REF}
+            VERSION=${GIT_VERSION}
+            # Debian versions for native packages can not contain hyphens
+            DEBIAN_VERSION=${VER}+git${REV}.${REF}
+        else
+            # Release version (e.g. 0.3.5)
+            VERSION=${GIT_VERSION}
+            DEBIAN_VERSION=${VERSION}
+        fi
     fi
 fi
 
