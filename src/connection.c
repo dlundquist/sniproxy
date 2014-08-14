dlundquist/sniproxy

commit cc666dc19a7ae744cebe7a6c06dd2a09f99cb985

Use libev for timestamps
Libev uses doubles internally for timekeeping, so there wasn't anything
to gain by tracking timestamps in timespec structs and using
clock_gettime reduces portability.

Fixes #94.

src/connection.c
@@ -633,39 +633,17 @@ new_connection() {
     return con;
 }
 
-/*
- * timespeccmp and timespecsub macros from OpenBSD sys/time.h
- */
-#ifndef timespeccmp
-#define timespeccmp(a, b, CMP)                      \
- (((a)->tv_sec == (b)->tv_sec) ?                    \
-  ((a)->tv_nsec CMP (b)->tv_nsec) :                 \
-  ((a)->tv_sec CMP (b)->tv_sec))
-#endif
-
-#ifndef timespecsub
-#define timespecsub(a, b, result)                   \
- do {                                               \
-   (result)->tv_sec = (a)->tv_sec - (b)->tv_sec;    \
-   (result)->tv_nsec = (a)->tv_nsec - (b)->tv_nsec; \
-   if ((result)->tv_nsec < 0) {                     \
-     --(result)->tv_sec;                            \
-     (result)->tv_nsec += 1000000000;               \
-   }                                                \
- } while (0)
-#endif
-
 static void
 log_connection(struct Connection *con) {
-    struct timespec duration;
+    ev_tstamp duration;
     char client_address[128];
     char listener_address[128];
     char server_address[128];
 
-    if (timespeccmp(&con->client.buffer->last_recv, &con->server.buffer->last_recv, >))
-        timespecsub(&con->client.buffer->last_recv, &con->established_timestamp, &duration);
+    if (con->client.buffer->last_recv > con->server.buffer->last_recv)
+        duration = con->established_timestamp - con->client.buffer->last_recv;
     else
-        timespecsub(&con->server.buffer->last_recv, &con->established_timestamp, &duration);
+        duration = con->established_timestamp - con->server.buffer->last_recv;
 
     display_sockaddr(&con->client.addr, client_address, sizeof(client_address));
     display_address(con->listener->address, listener_address, sizeof(listener_address));
@@ -673,7 +651,7 @@ log_connection(struct Connection *con) {
 
     log_msg(con->listener->access_log,
            LOG_NOTICE,
-           "%s -> %s -> %s [%.*s] %ld/%ld bytes tx %ld/%ld bytes rx %ld.%03ld seconds",
+           "%s -> %s -> %s [%.*s] %ld/%ld bytes tx %ld/%ld bytes rx %1.3f seconds",
            client_address,
            listener_address,
            server_address,
@@ -683,8 +661,7 @@ log_connection(struct Connection *con) {
            con->server.buffer->rx_bytes,
            con->client.buffer->tx_bytes,
            con->client.buffer->rx_bytes,
-           duration.tv_sec,
-           duration.tv_nsec / 1000000);
+           duration);
 }
 
 static void

configure.ac
@@ -50,17 +50,6 @@ AS_IF([test "x$dns" = "xyes"],
   ])
 ])
 
-PKG_CHECK_MODULES([LIBRT], [librt], HAVE_LIBRT=yes; AC_DEFINE(HAVE_LIBRT, 1),
-[AC_LIB_HAVE_LINKFLAGS(rt,, [#include <time.h>], [clock_gettime(CLOCK_MONOTONIC, NULL);])
- if test x$ac_cv_librt = xyes; then
-  AC_SUBST([LIBRT_LIBS], [$LIBRT])
- else
-  AC_MSG_ERROR([[***
-*** librt was not found.
-***]])
- fi
-])
-
 # Checks for header files.
 AC_FUNC_ALLOCA
 AC_CHECK_HEADERS([arpa/inet.h fcntl.h netdb.h netinet/in.h stddef.h stdlib.h string.h strings.h sys/socket.h sys/time.h syslog.h unistd.h],,

src/Makefile.am
@@ -1,4 +1,4 @@
-AM_CPPFLAGS = $(LIBEV_CFLAGS) $(LIBPCRE_CFLAGS) $(LIBRT_CFLAGS) $(LIBUDNS_CFLAGS)
+AM_CPPFLAGS = $(LIBEV_CFLAGS) $(LIBPCRE_CFLAGS) $(LIBUDNS_CFLAGS)
 
 sbin_PROGRAMS = sniproxy
 
@@ -36,4 +36,4 @@ sniproxy_SOURCES = address.c \
                    tls.c \
                    tls.h
 
-sniproxy_LDADD = $(LIBEV_LIBS) $(LIBPCRE_LIBS) $(LIBRT_LIBS) $(LIBUDNS_LIBS)
+sniproxy_LDADD = $(LIBEV_LIBS) $(LIBPCRE_LIBS) $(LIBUDNS_LIBS)

src/buffer.c
@@ -38,6 +38,7 @@
 #endif
 
 #include <assert.h>
+#include <ev.h>
 #include "buffer.h"
 #include "logger.h"
 
@@ -63,8 +64,8 @@ new_buffer(int size) {
     buf->head = 0;
     buf->tx_bytes = 0;
     buf->rx_bytes = 0;
-    buf->last_recv = (struct timespec){.tv_sec = 0, .tv_nsec = 0};
-    buf->last_send = (struct timespec){.tv_sec = 0, .tv_nsec = 0};
+    buf->last_recv = ev_time();
+    buf->last_send = ev_time();
     buf->buffer = malloc(buf->size);
     if (buf->buffer == NULL) {
         free(buf);
@@ -127,8 +128,7 @@ buffer_recv(struct Buffer *buffer, int sockfd, int flags) {
 
     bytes = recvmsg(sockfd, &msg, flags);
 
-    if (clock_gettime(CLOCK_MONOTONIC, &buffer->last_recv) < 0)
-        err("clock_gettime() failed: %s", strerror(errno));
+    buffer->last_recv = ev_time();
 
     if (bytes > 0)
         advance_write_position(buffer, bytes);
@@ -152,8 +152,7 @@ buffer_send(struct Buffer *buffer, int sockfd, int flags) {
 
     bytes = sendmsg(sockfd, &msg, flags);
 
-    if (clock_gettime(CLOCK_MONOTONIC, &buffer->last_send) < 0)
-        err("clock_gettime() failed: %s", strerror(errno));
+    buffer->last_send = ev_time();
 
     if (bytes > 0)
         advance_read_position(buffer, bytes);

src/buffer.h
@@ -28,7 +28,7 @@
 
 #include <stdio.h>
 #include <sys/types.h>
-#include <sys/time.h> /* struct timeval */
+#include <ev.h>
 
 
 struct Buffer {
@@ -36,8 +36,8 @@ struct Buffer {
     size_t size;
     size_t head;
     size_t len;
-    struct timespec last_recv;
-    struct timespec last_send;
+    ev_tstamp last_recv;
+    ev_tstamp last_send;
     size_t tx_bytes;
     size_t rx_bytes;
 };

src/connection.h
@@ -28,7 +28,6 @@
 
 #include <sys/socket.h>
 #include <sys/queue.h>
-#include <sys/time.h> /* struct timeval */
 #include <ev.h>
 #include "listener.h"
 #include "buffer.h"
@@ -56,7 +55,7 @@ struct Connection {
     const char *hostname; /* Requested hostname */
     size_t hostname_len;
     struct ResolvQuery *query_handle;
-    struct timespec established_timestamp;
+    ev_tstamp established_timestamp;
 
     TAILQ_ENTRY(Connection) entries;
 };

tests/Makefile.am
@@ -1,4 +1,4 @@
-AM_CPPFLAGS = -I$(top_srcdir)/src -g $(LIBEV_CFLAGS) $(LIBPCRE_CFLAGS) $(LIBRT_CFLAGS) $(LIBUDNS_CFLAGS)
+AM_CPPFLAGS = -I$(top_srcdir)/src -g $(LIBEV_CFLAGS) $(LIBPCRE_CFLAGS) $(LIBUDNS_CFLAGS)
 
 TESTS = http_test \
         tls_test \
@@ -45,7 +45,7 @@ binder_test_SOURCES = binder_test.c \
 buffer_test_SOURCES = buffer_test.c \
                       ../src/buffer.c
 
-buffer_test_LDADD = $(LIBRT_LIBS)
+buffer_test_LDADD = $(LIBEV_LIBS)
 
 address_test_SOURCES = address_test.c \
                       ../src/address.c
@@ -70,7 +70,7 @@ config_test_SOURCES = config_test.c \
                       ../src/tls.c \
                       ../src/http.c
 
-config_test_LDADD = $(LIBEV_LIBS) $(LIBPCRE_LIBS) $(LIBRT_LIBS) $(LIBUDNS_LIBS)
+config_test_LDADD = $(LIBEV_LIBS) $(LIBPCRE_LIBS) $(LIBUDNS_LIBS)
 
 resolv_test_SOURCES = resolv_test.c \
                       ../src/resolv.c \

commit ff80d6132ff4959078dd614d23af1317288f2055

Fix negative connection durations in access log
Regression introduced in cc666dc1.

src/connection.c
@@ -640,9 +640,9 @@ log_connection(struct Connection *con) {
     char server_address[128];
 
     if (con->client.buffer->last_recv > con->server.buffer->last_recv)
-        duration = con->established_timestamp - con->client.buffer->last_recv;
+        duration = con->client.buffer->last_recv - con->established_timestamp;
     else
-        duration = con->established_timestamp - con->server.buffer->last_recv;
+        duration = con->server.buffer->last_recv - con->established_timestamp;
 
     display_sockaddr(&con->client.addr, client_address, sizeof(client_address));
     display_address(con->listener->address, listener_address, sizeof(listener_address));