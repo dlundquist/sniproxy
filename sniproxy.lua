e1be7f4676aa597b26ca627ad6eabd925ec46d2f
Peter van Dijk <peter.van.dijk@powerdns.com>
2017-02-07 20:37:12 +0100

    stop hexdumping remote now that it's in presentation format

--- sniproxy.lua
+++ sniproxy.lua
@@ -1,16 +1,5 @@
-function hex_dump(buf)
-  for byte=1, #buf, 16 do
-    local chunk = buf:sub(byte, byte+15)
-    io.write(string.format('%08X  ',byte-1))
-    chunk:gsub('.', function (c) io.write(string.format('%02X ',string.byte(c))) end)
-    io.write(string.rep(' ',3*(16-#chunk)))
-    io.write("\n")
-  end
-end
-
 function preconnect(remote, name)
   print('->')
-  hex_dump(remote)
   print("["..remote.."]")
   print("["..name.."]")
   name = string.lower(name)
