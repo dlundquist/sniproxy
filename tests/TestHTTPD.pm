3da4e54207f1e68ec0b4d9abf7585d0278611f30
Dustin Lundquist <dustin@null-ptr.net>
2018-03-01 20:15:35 -0800

    tests: proxy_header_test

--- tests/TestHTTPD.pm
+++ tests/TestHTTPD.pm
@@ -10,6 +10,35 @@ our @ISA = qw(Exporter);
 our @EXPORT = qw(new);
 our $VERSION = '0.01';
 
+my $http_methods = {
+    'GET' => 1,
+    'POST' => 1,
+    'HEAD' => 1,
+    'PUT' => 1,
+    'DELETE' => 1,
+    'TRACE' => 1,
+    'DEBUG' => 1,
+    'CONNECT' => 1,
+    'OPTIONS' => 1,
+};
+
+
+sub default_response_parser {
+    my $sock = shift;
+    my $status = 500;
+
+    # Read HTTP request
+    for (my $i = 0; my $line = $sock->getline(); $i++) {
+        if ($i == 0 && $line =~ m/\A(\S+) (\S+) HTTP\/(\S+)\r\n\z/) {
+            $status = 200 if exists($http_methods->{$1});
+        }
+
+        # Wait for blank line indicating the end of the request
+        last if $i > 0 && $line eq "\r\n";
+    }
+
+    return $status;
+};
 
 my $count = 0;
 
@@ -22,17 +51,24 @@ my $responses = [
     [ 20, 1, 1, 1, 1, 1, 1, 200 ],
 ];
 
+my $http_status_line = {
+    200 => 'OK',
+    203 => 'Non-Authoritative Information',
+    500 => 'Internal Server Error',
+};
+
 sub default_response_generator {
     $count ++;
 
-    return sub($) {
+    return sub($$) {
         my $sock = shift;
+        my $status = shift;
 
         my @chunks = @{$responses->[$count % scalar @{$responses}]};
         my $content_length = 0;
         map { $content_length += $_ } @chunks;
 
-        print $sock "HTTP/1.1 200 OK\r\n";
+        print $sock "HTTP/1.1 $status " . $http_status_line->{$status} . "\r\n";
         print $sock "Server: TestHTTPD/$VERSION\r\n";
         print $sock "Content-Type: text/plain\r\n";
         print $sock "Content-Length: $content_length\r\n";
@@ -52,6 +88,7 @@ sub httpd {
     my %args = @_;
     my $ip = $args{'ip'} || 'localhost';
     my $port = $args{'port'} || 8081;
+    my $request_parser = $args{'parser'} || \&default_response_parser;
     my $responder_generator = $args{'generator'} || \&default_response_generator;
 
     my $server = IO::Socket::INET->new(Listen    => Socket::SOMAXCONN(),
@@ -72,14 +109,10 @@ sub httpd {
 
         # Child
         #
-        # Read HTTP request
-        while (my $line = $client->getline()) {
-            # Wait for blank line indicating the end of the request
-            last if $line eq "\r\n";
-        }
+        my $status = $request_parser->($client);
 
         # Assume a GET request
-        $responder->($client);
+        $responder->($client, $status);
 
         $client->close();
         exit 0;
