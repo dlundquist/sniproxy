ee0660bcca7efa0513c645e9f212101fdb4dddd5
Kyle Mestery <mestery@mestery.com>
2020-04-09 10:01:52 -0500

    IPv6: Compile on macOS
    We need to define __APPLE_USE_RFC_3542 before including <netinet/in.h> or
    compile errors are observed on macOS.
    
    Signed-off-by: Kyle Mestery <mestery@mestery.com>

--- src/connection.c
+++ src/connection.c
@@ -32,6 +32,9 @@
 #include <sys/queue.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#ifdef __APPLE__
+#define __APPLE_USE_RFC_3542
+#endif
 #include <netinet/in.h>
 #include <netdb.h> /* getaddrinfo */
 #include <unistd.h> /* close */
--- src/listener.c
+++ src/listener.c
@@ -37,6 +37,9 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/un.h>
+#ifdef __APPLE__
+#define __APPLE_USE_RFC_3542
+#endif
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <assert.h>
